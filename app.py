353f67dda4722614cf9b4198e7233705a4584376
viivizz <viivi.roikonen@gmail.com>
2026-05-20 15:28:51 +0300

    Show item list/info

--- app.py
+++ app.py
@@ -14,7 +14,14 @@ app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    all_items=items.get_items()
+    return render_template("index.html", items=all_items)
+
+@app.route("/item/<int:item_id>")
+def show_item(item_id):
+    item=items.get_item(item_id)
+    return render_template("show_item.html", item=item)
+
 
 @app.route("/new_item")
 def new_item():
--- items.py
+++ items.py
@@ -4,3 +4,17 @@ def add_item(title, review, info, user_id):
     sql = """INSERT INTO items (title, review, info, user_id)
             VALUES (?, ?, ?, ?)"""
     db.execute(sql, [title, review, info, user_id])
+
+def get_items():
+    sql = "SELECT id, title FROM items ORDER BY id DESC"
+    return db.query(sql)
+
+def get_item(item_id):
+    sql = """SELECT items. title,
+                    items.review,
+                    items.info,
+                    users.username
+            FROM items, users
+            WHERE items.user_id = users.id AND
+                items.id = ?"""
+    return db.query(sql, [item_id])[0]
