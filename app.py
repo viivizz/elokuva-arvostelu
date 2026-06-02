5fcd34f50a03913beb65a16324a95df1877c666b
viivizz <viivi.roikonen@gmail.com>
2026-06-02 16:51:52 +0300

    Add user pages

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import db
 import config
 import items
+import users
 
 
 
@@ -19,6 +20,15 @@ def require_login():
 def index():
     all_items=items.get_items()
     return render_template("index.html", items=all_items)
+
+@app.route("/user/<int:user_id>")
+def show_user(user_id):
+    user=users.get_user(user_id)
+    if not user:
+        abort(404)
+    items=users.get_items(user_id)
+    return render_template("show_user.html", user=user, items=items)
+
 
 @app.route("/find_item")
 def find_item():
